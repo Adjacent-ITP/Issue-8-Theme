d8142e5741fb9f7f5fd0cdf75f09db006c8bfa98
evanzummeren <erikvanzummeren@gmail.com>
2021-02-07 14:47:29 -0500

    add blog nav

--- single.php
+++ single.php
@@ -18,12 +18,7 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'issue-8' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'issue-8' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+
 
 		endwhile; // End of the loop.
 		?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -13,7 +13,19 @@
 	<div class="column article">
 
 		<nav class="article__nav">
-
+		<?php the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav__arrowleft"></span><span class="nav-subtitle">' . esc_html__( 'Previous', 'issue-8' ) . '</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next', 'issue-8' ) . '</span>',
+				)
+			); ?>
+			<?php wp_link_pages(
+					array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'issue-8' ),
+						'after'  => '</div>',
+					)
+				);
+			?>
 		</nav>
 
 		<div class="content">
@@ -56,12 +68,6 @@
 					)
 				);
 
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'issue-8' ),
-						'after'  => '</div>',
-					)
-				);
 				?>
 				</div>
 
