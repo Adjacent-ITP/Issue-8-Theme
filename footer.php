8b5d0522fad069052859be3462671d81ee3167a9
Sue_PC <sueroh@gmail.com>
2021-08-13 14:29:57 -0400

    Fixed cutting off on top and bottom of images

--- footer.php
+++ footer.php
@@ -12,9 +12,11 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-		<span><span class="footer__title">ADJACENT</span> Issue 8, published by ITP & IMA, Tisch School of the Arts, New York University. <a href="https://www.nyu.edu/footer/accessibility.html" target="_blank">Accessibility</a>. <a href="https://gmail.us20.list-manage.com/subscribe?u=3bf5cb27bc2d1fac602917e9b&id=83eea1073c" target="_blank">Subscribe to the newsletter.</a></span>
-		</div><!-- .site-info -->
+	<span><span class="footer__title">ADJACENT</span>
+			<span class="footer_text"> 
+				Issue 8, published by ITP & IMA, Tisch School of the Arts, New York University. <a href="https://www.nyu.edu/footer/accessibility.html" target="_blank">Accessibility</a>. <a href="https://gmail.us20.list-manage.com/subscribe?u=3bf5cb27bc2d1fac602917e9b&id=83eea1073c" target="_blank">Subscribe to the newsletter.</a>
+</span>
+			</span>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
