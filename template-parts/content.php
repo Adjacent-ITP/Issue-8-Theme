7538269b02b5a8d5985ef7a309e5bb1a664b9553
Sue_PC <sueroh@gmail.com>
2021-08-19 16:00:08 -0400

    Fixing side illustration

--- template-parts/content.php
+++ template-parts/content.php
@@ -178,7 +178,7 @@
 		</script>
 	<?php elseif(get_field('layout_type') == "vimeo"): ?>
 		<div class='column artwork illustrations'>
-			<div style="padding:100% 0 0 0;position:relative;"><iframe src="<?php the_field('iframe_src'); ?>" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+			<div style="padding:100% 0 0 0;position:relative;"><iframe src="<?php the_field('iframe_src'); ?>" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 		</div>
 	<?php elseif(get_field('layout_type') == "iframe"): ?>
 		<div class="column artwork interactivepiece">
